a6bc75ef179876d3bcfe298852672a86fe0cfd16
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-08-24 00:31:18 +0000

    Deploy tinyurl.geekofia.in to github.com/chankruze/TinyURL-Website.git:gh-pages

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,8 +6,17 @@ Copyright (c) Geekofia 2020 and beyond
 */
 
 $(function () {
-    $(document).scroll(function () {
-      let $nav = $(".navbar");
-      $nav.toggleClass('scrolled', $(this).scrollTop() >= $nav.height());
-    });
+  $(document).scroll(function () {
+    let $nav = $(".navbar");
+    $nav.toggleClass('scrolled fixed-top', $(this).scrollTop() >= $nav.height());
   });
+});
+
+$('.more').click(function () {
+  $(this).toggleClass("fa-angle-down fa-angle-up");
+  $(this).parent().parent().siblings(".command-wrapper").toggle();
+
+  // close other commands' more
+  $('.more.fa-angle-up').not(this).toggleClass("fa-angle-up fa-angle-down");
+  $('.more').not(this).parent().parent().siblings(".command-wrapper").hide();
+});
